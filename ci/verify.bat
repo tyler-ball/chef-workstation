ece440287701f6e1ada6f80e4b707fa8885b4607
Scott Hain <shain@chef.io>
2018-06-19 16:30:58 -0700

    Add call for thing
    Signed-off-by: Scott Hain <shain@chef.io>

--- ci/verify.bat
+++ ci/verify.bat
@@ -5,16 +5,13 @@ REM ; package is working correctly after install.
 
 REM ; chef-run version ensures our bin ends up on path and the basic ruby env is
 REM ; working.
-chef-run --version
+call chef-run --version
 IF %ERRORLEVEL% NEQ 0 goto :error
 
 REM ; Ensure our ChefDK works
-chef env
+call chef env
 IF %ERRORLEVEL% NEQ 0 goto :error
 
 REM ; Test
-chef-run version
-IF %ERRORLEVEL% NEQ 0 goto :error
-
-jklfds fail
+call chef-run version
 IF %ERRORLEVEL% NEQ 0 goto :error
