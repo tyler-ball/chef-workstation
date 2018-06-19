ac7885861b93edfd3693e1ee7d1da3a4a06fbc53
Scott Hain <shain@chef.io>
2018-06-19 13:48:54 -0700

    add some debugging
    Signed-off-by: Scott Hain <shain@chef.io>

--- ci/verify.bat
+++ ci/verify.bat
@@ -1,12 +1,17 @@
+@echo off
+
 REM ; This script is used in the jenkins part of our pipeline to verify our
 REM ; package is working correctly after install.
 
 REM ; chef-run version ensures our bin ends up on path and the basic ruby env is
 REM ; working.
-chef-run --version || echo "Exited with error %errorlevel%" && exit /b 1
+chef-run --version
+call chef-run --version
 
 REM ; Ensure our ChefDK works
-chef env || echo "Exited with error %errorlevel%" && exit /b 2
+chef env
+call chef env
 
 REM ; Test
-chef-run version || echo "Exited with error %errorlevel%" && exit /b 3
+chef-run version
+call chef-run version
